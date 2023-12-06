6a12e23e87af337c7b82f1242eb8300030e879a9
afahid <a.fehid@elarabygroup.com>
2023-12-06 08:54:25 +0200

    knex

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,19 +3,17 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 const knexSnakeCaseMappers = require('objection');
+const dotenv = require('dotenv').config();
 
 module.exports = {
   development: {
     client: 'pg',
     connection: {
       database: 'objection_tutorial',
-      user: 'postgres',
-      password: 'postgres',
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+
     migrations: {
       tableName: 'knex_migrations',
     },
